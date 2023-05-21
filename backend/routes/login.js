cd4ae212602a51c38f4eca8f94ca7ddcb709fb33
dev-002 <devansh.gupta985@gmail.com>
2023-05-21 16:21:07 +0530

    Simple User Auth

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -4,12 +4,22 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('signForm');
-})
+});
 
 router.post('/create', async (req, res) => {
-    const user = await User.create({...req.body.user});
+    const user = await User.create({ ...req.body.user });
     await user.save();
     res.send(user);
+});
+
+router.post('/verify', async (req, res) => {
+    const user = await User.findOne({ name: req.body.user.name });
+    if (req.body.user.password == user.password) {
+        res.send('Welcome ' + user.name);
+    }
+    else {
+        res.redirect('/login');
+    }
 })
 
 module.exports = router;
